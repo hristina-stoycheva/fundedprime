6e4d0650d644adc5c52c035ec53858b59657e22e
hristina-stoycheva <hristina.stoycheva@eightcap.com>
2025-01-14 10:13:30 +0200

    footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,9 +5,21 @@ import "./styles/Footer.css";
 import { fetchACFData } from "../api";
 const Footer = () => {
   return (
-    <div className="container">
-      <Menu />
-      <FooterMenu />
+    <div className="footer container">
+      <div className="footer_top_line row">
+        <div className="col-8 menu_topline_footer">
+        <Menu />
+        </div>
+        <div className="col-4 footer_top_right">
+        <div className="logo_topline_footer">Logo</div>
+        <div className="footer_top_text">email</div>
+        </div>
+      </div>
+      <div className="footer_top_line row">
+        <div className="footer_bottom_menu"><FooterMenu /></div>
+        <div className="footer_bottom_text">text</div>
+        
+      </div>
     </div>
   );
 };
